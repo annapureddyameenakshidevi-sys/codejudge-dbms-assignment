5997d1641b13b10a7d77bc65f59f2b18efcd46f8
annapureddyameenakshidevi-sys <annapureddyameenakshidevi@gmail.com>
2026-05-19 19:02:53 +0530

    Create sample_queries.sql

---
+++ sample_queries.sql
@@ -0,0 +1,15 @@
+INSERT INTO students VALUES
+(1, 'Rahul', 'rahul@gmail.com'),
+(2, 'Sneha', 'sneha@gmail.com');
+
+INSERT INTO courses VALUES
+(101, 'DBMS'),
+(102, 'Python');
+
+INSERT INTO enrollments VALUES
+(1, 1, 101),
+(2, 2, 102);
+
+SELECT * FROM students;
+SELECT * FROM courses;
+SELECT * FROM enrollments;
